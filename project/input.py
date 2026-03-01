ca7bb3e3f1c19a2fe6bc91594e75098d9635dcce
G. Borra <g.borra001@studenti.unibs.it>
2026-03-01 14:48:58 +0100

    aggiunto import dei parametri

--- project/input.py
+++ project/input.py
@@ -1,8 +1,10 @@
 import numpy as np
-
-import parameters.Parameters as Param
+import importlib
+from pathlib import Path
 
 def main():
+    Param = importlib.import_module(f"{getParam()}.Parameters")
+    
     param = Param.Parameters()
     
     #Given theta (distribution of the hat and check population on the routes):
@@ -21,6 +23,23 @@ def main():
     
     print(T_hat)
     print(T_check)
+    
+def getParam():
+    cartella = Path(__file__).parent
+    parametri = [f.name for f in cartella.iterdir() if f.is_dir and f.name.startswith("parameters")]
+    print(parametri)
+    
+    scelta = None
+    while scelta is None:
+        try:
+            indice = int(input("Scegli un pacchetto (numero): ")) - 1
+            if 0 <= indice < len(parametri):
+                scelta = parametri[indice]
+            else:
+                print("Numero non valido. Riprova.")
+        except ValueError:
+            print("Inserisci un numero valido.")
+    return scelta
 
 if __name__ == "__main__":
     main()
